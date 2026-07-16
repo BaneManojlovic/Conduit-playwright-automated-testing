0194c45991375df4d25fb82badcf740b59bfa828
BranislavManojlovic <branislav.manojlovic@vegait.rs>
2026-07-16 14:54:52 +0200

    CON-001: Initialized Playwright test project and added trial test.

---
+++ tests/example.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://conduit.bondaracademy.com/');
+
+  const title = await page.locator('.logo-font');
+
+  // Expect a title "to contain" a substring.
+  await expect(title).toHaveText('conduit');
+});
+
